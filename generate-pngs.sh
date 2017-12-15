9cec61b5fdea62ed692e2eaaf80c0811f96b9b3f
Ellen Marie Dash <me@duckie.co>
2017-12-15 05:46:00 -0500

    add script for converting PNG to a collection of PNGs.

---
+++ generate-pngs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf pngs
+mkdir -p pngs
+cd pngs
+
+convert -crop 16x16 +repage ../awoof.png awoof-%d.png
+
+for x in $(find -type f); do
+  OLD=$x
+  NEW=$(echo $x | sed 's/\-\([0-9][0-9]\)\./-0\1./' | sed 's/\-\([0-9]\)\./-00\1./')
+
+  mv $OLD $NEW
+done
